aec7fe0e56c88c34fc644744dff4a5b4f41e857e
MathurinChrist <121124199+MathurinChrist@users.noreply.github.com>
2026-04-23 11:18:02 +0200

    Revert "Feature/snack video"

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
-import { createApp } from "vue";
-import "./index.css";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import './index.css'
+import App from './App.vue'
 
-createApp(App).use(router).mount("#app");
+createApp(App).mount('#app')
